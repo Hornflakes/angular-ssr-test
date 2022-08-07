f7e79653e7686765e3b39b73e3cf363b4b0ef00e
Hornflakes <cristianpetrugug@gmail.com>
2022-08-07 20:46:17 +0300

    configured SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    SharedModule
+    SharedModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' })
   ],
   providers: [],
   bootstrap: [AppComponent]
